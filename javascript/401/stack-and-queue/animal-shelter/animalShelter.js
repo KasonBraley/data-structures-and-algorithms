0032860a006c820aceae1fbdc40680d9d8082b32
kason <kbraley9@gmail.com>
2021-11-02 13:59:31 -0700

    Add enqueue animal validation

--- javascript/401/stack-and-queue/animal-shelter/animalShelter.js
+++ javascript/401/stack-and-queue/animal-shelter/animalShelter.js
@@ -9,6 +9,10 @@ class AnimalShelter {
   }
 
   enqueue(animal) {
+    if (animal.animal !== "dog" || animal.animal !== "cat") {
+      throw new Error("Valid animals are 'dog' or 'cat'")
+    }
+
     if (animal.animal === "dog") {
       this.#dogs.enqueue(animal)
       return
