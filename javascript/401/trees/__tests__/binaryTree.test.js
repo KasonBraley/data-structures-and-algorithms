2ec4b8f7047bc041c9e662bf628e8edbd73e27c0
kason <kbraley9@gmail.com>
2021-11-05 16:12:00 -0700

    (test): Add test cases

---
+++ javascript/401/trees/__tests__/binaryTree.test.js
@@ -0,0 +1,12 @@
+const Node = require("../Node.js")
+const BinaryTree = require("../BinaryTree.js")
+const { describe, expect, test } = require("@jest/globals")
+
+describe("BinaryTree", () => {
+  test("Can instantiate an empty tree", () => {})
+  test("Can instantiate a tree with a single root node", () => {})
+  test("Can add a left and right child to a single root node", () => {})
+  test("Can return a collection from a pre-order traversal", () => {})
+  test("Can return a collection from a in-order traversal", () => {})
+  test("Can return a collection from a post-order traversal", () => {})
+})
