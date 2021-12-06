b7f0d977e56011bd06f83a362e702b06d1431b09
kason <kbraley9@gmail.com>
2021-12-05 21:59:41 -0700

    Add implementation

--- 401/hashtable/leftJoin.js
+++ 401/hashtable/leftJoin.js
@@ -1,5 +1,14 @@
-function leftJoin(hashMap1, hashMap2){
+const HashTable = require("./hashTable.js")
 
+function leftJoin(hashMap1, hashMap2) {
+  let arr = []
+
+  hashMap1.map.forEach((value) => {
+    Object.getOwnPropertyNames(value.head.value).forEach((prop) => {
+      arr.push([prop, hashMap1.get(prop), hashMap2.get(prop)])
+    })
+  })
+
+  return arr
 }
-
 module.exports = leftJoin
