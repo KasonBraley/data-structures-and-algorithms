7590ac415343176411b70f433e23adf41579297c
kason <kbraley9@gmail.com>
2021-12-07 23:30:57 -0700

    Add getSize() test

--- 401/graphs/__tests__/graph.test.js
+++ 401/graphs/__tests__/graph.test.js
@@ -9,4 +9,18 @@ describe("Graph implementation", () => {
     graph.addVertex(A)
     expect(graph.adjacencyList.size).toBe(1)
   })
+  test("The proper size is returned, representing the number of nodes in the graph", () => {
+    let graph = new Graph()
+
+    let A = new Vertex("A")
+    let B = new Vertex("B")
+    let C = new Vertex("C")
+    let D = new Vertex("D")
+    graph.addVertex(A)
+    graph.addVertex(B)
+    graph.addVertex(C)
+    graph.addVertex(D)
+
+    expect(graph.getSize()).toBe(4)
+  })
 })
