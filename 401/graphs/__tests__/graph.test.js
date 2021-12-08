b8afec45b5dc0a0d5802fd7f581ece9b9eb0cf4c
kason <kbraley9@gmail.com>
2021-12-08 12:52:43 -0700

    Add test for adding an edge

--- 401/graphs/__tests__/graph.test.js
+++ 401/graphs/__tests__/graph.test.js
@@ -9,6 +9,20 @@ describe("Graph implementation", () => {
     graph.addVertex(A)
     expect(graph.adjacencyList.size).toBe(1)
   })
+
+  test("An edge can be successfully added to the graph", () => {
+    let graph = new Graph()
+
+    let A = new Vertex("A")
+    let B = new Vertex("B")
+    graph.addVertex(A)
+    graph.addVertex(B)
+    graph.addDirectedEdge(A, B)
+    expect(graph.adjacencyList.get(A)).toMatchObject([
+      { vertex: { value: "B" }, weight: 0 },
+    ])
+  })
+
   test("A collection of all nodes can be properly retrieved from the graph", () => {
     let graph = new Graph()
 
