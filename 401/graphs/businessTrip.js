98bf5bd440acb7b142157fd8a44879200ce4e92c
kason <kbraley9@gmail.com>
2021-12-09 17:03:30 -0700

    (feat): Add implementation

---
+++ 401/graphs/businessTrip.js
@@ -0,0 +1,23 @@
+// Determine whether the trip is possible with direct flights, and how much it would cost.
+
+function businessTrip(graph, cities) {
+  let cost = null
+
+  for (let i = 0; i < cities.length; i++) {
+    let contains = graph.getNeighbors(cities[i]).some((city) => {
+      return city.vertex.value === cities[i + 1]?.value
+    })
+
+    if (contains) {
+      let directFlight = graph.getNeighbors(cities[i]).filter((city) => {
+        return city.vertex.value === cities[i + 1]?.value
+      })[0]
+
+      cost += directFlight.weight
+    }
+  }
+
+  return cost
+}
+
+module.exports = businessTrip
